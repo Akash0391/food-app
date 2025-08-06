c0d96716c4c967b9286c93eb4e8daf1b988e86d0
Thinkbox <akashjaunpur0391@gmail.com>
2025-08-06 13:33:28 +0530

    feat: add routing and pages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-
 import { SafeAreaView } from "react-native-safe-area-context";
 import cn from "clsx";
 import CartButton from "@/components/CartButton";
-
+  
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
