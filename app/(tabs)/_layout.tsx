4839110a4d28e9472f0aac9b1facf07b2455e9db
Thinkbox <akashjaunpur0391@gmail.com>
2025-08-07 15:25:47 +0530

    feat: add tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,31 @@
 import { useAuthStore } from "@/store/auth.store";
-import { Redirect, Slot } from "expo-router";
+import { Redirect, Slot, Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { View, Text, Image } from "react-native";
+import { TabBarIconProps } from "@/type";
+import { images } from "@/constants";
+import cn from "clsx";
 
-export default function TabsLayout() {  
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+  <View className="tab-icon">
+    <Image
+      source={icon}
+      className="size-7"
+      resizeMode="contain"
+      tintColor={focused ? "#FE8C00" : "#5D5F6D"}
+    />
+    <Text
+      className={cn(
+        "text-sm font-bold",
+        focused ? "text-primary" : "text-gray-200"
+      )}
+    >
+      {title}
+    </Text>
+  </View>
+);
+
+export default function TabsLayout() {
   const { isAuthenticated } = useAuthStore();
 
   console.log("isAuthenticated", isAuthenticated);
@@ -10,5 +34,65 @@ export default function TabsLayout() {
     return <Redirect href="/sign-in" />;
   }
 
-  return <Slot />;
+  return (
+    <Tabs 
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          borderBottomRightRadius: 50,
+          marginHorizontal: 20,
+          height: 80,
+          position: "absolute",
+          bottom: 30,
+          backgroundColor: "white",
+          shadowColor: "#1a1a1a",
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.home} title="Home" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.search} title="Search" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.bag} title="Cart" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} icon={images.person} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
